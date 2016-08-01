14d7ca3336f71e45e56e01972c62491ef63df2b1
linlymatsumura <linly.matsumura@gmail.com>
2016-08-01 15:33:42 +0900

    gulpfile update

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,29 @@
-var gulp = require("gulp");
+var gulp = require('gulp');
+var rename = require('gulp-rename');
+var plumber = require('gulp-plumber');
+var notify  = require('gulp-notify');
+var uglify  = require('gulp-uglify');
 var babel = require("gulp-babel");
 
-gulp.task('babel', function() {
-  gulp.src('./src/*.js')
-    .pipe(babel())
-    .pipe(gulp.dest('./dist/'))
+gulp.task('js', function(){
+    gulp.src('./src/jquery.bem.es6')
+        .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
+        .pipe(babel())
+        .pipe(uglify())
+        .pipe(rename('jquery.bem.min.js'))
+        .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('debug', function(){
+    gulp.src('./src/jquery.bem.es6')
+        .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
+        .pipe(babel())
+        .pipe(rename('jquery.bem.min.js'))
+        .pipe(gulp.dest('./dist/'));
 });
 
 gulp.task('watch', function() {
-  gulp.watch('./src/*.js', ['babel'])
+  gulp.watch('./src/jquery.bem.es6', ['js'])
 });
 
-gulp.task('default', ['babel', 'watch']);
+gulp.task('default', ['js']);
